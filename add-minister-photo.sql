8e817e1778b442d54d9736b9f8e1ec56db5ce6f5
Mohd Shahzwann <wannwanie@icloud.com>
2025-07-11 17:14:00 +0800

    refactor: remove minister photo field addition from official page content

--- add-minister-photo.sql
+++
@@ -1,8 +0,0 @@
--- ===============================================
--- ADD MINISTER PHOTO FIELD TO OFFICIAL PAGE CONTENT
--- Run this to add minister photo support
--- ===============================================
-
--- Add minister photo URL field to existing table
-ALTER TABLE official_page_content 
-ADD COLUMN IF NOT EXISTS minister_photo_url TEXT DEFAULT NULL;
